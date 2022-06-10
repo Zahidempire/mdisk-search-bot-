ec180dfd7b61985cfe605eafb03f8cd05eaf632f
CyniteOfficial <107128247+CyniteOfficial@users.noreply.github.com>
2022-06-10 19:50:58 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -62,6 +62,4 @@ Bots are simply Telegram accounts operated by software – not people – and t
 <b>👋 Hey !{} 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href='https://t.me/Mdisk_SearchRobot'>Mᴅɪsᴋ Sᴇᴀʀᴄʜ Rᴏʙᴏᴛ</a>
 
 <a> 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙼𝙰𝙺𝙴 𝙼𝙴 𝙰𝙳𝙼𝙸𝙽.. 𝚃𝙷𝙴𝙽 𝚂𝙴𝙴 𝙼𝚈 𝙿𝙾𝚆𝙴𝚁𝚂 ♥️♥️🔥
-    ADD_BOTS = """<b>Heya! {} If You Want to Add Your Bots In @Cyniteofficial then Contact Admin From Below Given Groups</b>"""
-
 
